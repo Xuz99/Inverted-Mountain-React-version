f5bc49025193736c2bf507a2bdcd53be761fc91f
John Davidson <xuz99@live.co.uk>
2020-10-27 21:12:00 +0000

    Refactored games components

---
+++ src/components/Games.js
@@ -0,0 +1,69 @@
+import React, {Fragment} from 'react';
+
+import placeholder_image_one from '../images/bg-placeholder-one.svg';
+import placeholder_image_two from '../images/bg-placeholder-two.svg';
+import placeholder_image_three from '../images/bg-placeholder-three.svg';
+
+const subheading_clases = "font-in-red text-md text-left xl:text-4xl"
+const text_clases = "xl:text-2xl leading-7 tracking-track-01 mb-8"
+const image_heading_clases = "text-4xl text-center text-white p-8 leading-none tracking-tiny-wide uppercase lato fw-400 break-words"
+const image_subheading_clases = "font-in-red text-lg text-center xxl:text-4xl mb-5"
+
+const Game = (props) =>  (
+	<h2 className="all_ties_down mx-3 my-10 xl:max-w-full xl:mx-auto xl:mb-10 lato-thin uppercase text-center text-4xl xl:text-6xl shadow-lg lato fw-300">
+	{props.title}
+	</h2>
+	<h2 className={subheading_clases}>
+	YEAR OF RELEASE
+	</h2>
+	<p className={text_clases}>
+	{props.year_of_release}
+	</p>
+	<h2 className={subheading_clases}>
+	GENRES
+	</h2>
+	<p className={text_clases}>
+	{props.genres}
+	</p>
+	<h2 className={subheading_clases}>
+	WHAT IT'S ALL ABOUT
+	</h2>
+	<p className={text_clases}>
+	{props.what_its_all_about}
+	</p>
+
+	<h2 className={image_heading_clases}>
+	EXPLORE THE LAND
+	</h2>
+	<h3 className={image_subheading_clases}>
+	CAN YOU SPEAK TO CRYSTALS?
+	</h3>
+	<img src={placeholder_image_one}
+	alt="A lovely background" 
+	className="border-8 border-in-grey shadow-lg">
+	</img>
+	<h2 className={image_heading_clases}>
+	FIGHT THE SYSTEM
+	</h2>
+	<h3 className={image_subheading_clases}>
+	ITS SERVANTS: DARK GUARDS & EXTRACTORS
+	</h3>
+	
+	<img src={placeholder_image_two}
+	alt="A lovely background" 
+	className="border-8 border-in-grey shadow-lg">
+	</img>
+	<h2 className={image_heading_clases}>
+	SOLVE THE MYSTERY
+	</h2>
+	<h3 className={image_subheading_clases}>
+	WHO'S THE MOTHER OF THIS PLANET?
+	</h3>
+	<img src={placeholder_image_three}
+	alt="A lovely background" 
+	className="border-8 border-in-grey shadow-lg">
+	</img>
+	)
+
+export default Game 
+
--- src/components/Play.js
+++ src/components/Play.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Alltiesdown from './games/Alltiesdown';
-import Iloveyou from './games/Iloveyou';
+import Game from './Games';
+
+// import Iloveyou from './games/Iloveyou';
 
 const all_ties_down_text = (`Equipped with a black tie and a white collar shirt, you answer e-mails sitting in the cubicle which you have learnt to call home. Then, as has become a habit of yours, you fall into the embrace of a daydream, closing your eyes, and leaving this reality away. 
   And when you open your eyes again, you are in a different place entirely: in the woods, 
@@ -10,6 +11,8 @@ const all_ties_down_text = (`Equipped with a black tie and a white collar shirt,
   whispers to you from the depths of your soul, you agree, and your journey into the 
   unconscious begins...`)
 
+  
+
 const Play = (props) =>  (
   <section className="text-white my-32">
   <header>
@@ -18,12 +21,12 @@ const Play = (props) =>  (
   <article className="xl:max-w-full xl:mx-64 xl:mb-48 lato fw-300 text-gray-400">
   <section>
 
-  <Alltiesdown title={'All\nTies\nDown'}
+  <Game title={'All\nTies\nDown'}
   year_of_release="Work in progress. We plan to release the early access version around X-mas 2020." 
   genres="Exploration turn-based mystery story-driven female-character counter-cultural" 
   what_its_all_about={all_ties_down_text} />
 
-  <Iloveyou title="I LOVE YOU, THEREFORE I NEED YOU TO DIE...AGAIN"
+  <Game title="I LOVE YOU, THEREFORE I NEED YOU TO DIE...AGAIN"
   year_of_release="2020, Christmas" 
   genres="Story-driven metaphysical shamanic" 
   what_its_all_about="Description..." />
