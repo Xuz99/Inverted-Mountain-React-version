6467bae71f7df23344d52f7539ce2ffd8e2a656c
John Davidson <xuz99@live.co.uk>
2020-10-30 23:41:19 +0000

    Trying to refactor IF - ELSE

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
 import placeholder_image_one from '../images/bg-placeholder-one.svg';
 import placeholder_image_two from '../images/bg-placeholder-two.svg';
@@ -9,8 +9,10 @@ const text_clases = "xl:text-2xl leading-7 tracking-track-01 mb-8"
 const image_heading_clases = "text-4xl text-center text-white p-8 leading-none tracking-tiny-wide uppercase lato fw-400 break-words"
 const image_subheading_clases = "font-in-red text-lg text-center xxl:text-4xl mb-5"
 
+
 const Game = (props) =>  (
-	<h2 className="all_ties_down mx-3 my-10 xl:max-w-full xl:mx-auto xl:mb-10 lato-thin uppercase text-center text-4xl xl:text-6xl shadow-lg lato fw-300">
+	<div>
+	<h2 className={props.custom_class + " mx-3 my-10 xl:max-w-full xl:mx-auto xl:mb-10 lato-thin uppercase text-center text-4xl xl:text-6xl shadow-lg lato fw-300"}>
 	{props.title}
 	</h2>
 	<h2 className={subheading_clases}>
@@ -31,39 +33,44 @@ const Game = (props) =>  (
 	<p className={text_clases}>
 	{props.what_its_all_about}
 	</p>
+)
 
-	<h2 className={image_heading_clases}>
-	EXPLORE THE LAND
-	</h2>
-	<h3 className={image_subheading_clases}>
-	CAN YOU SPEAK TO CRYSTALS?
-	</h3>
-	<img src={placeholder_image_one}
-	alt="A lovely background" 
-	className="border-8 border-in-grey shadow-lg">
-	</img>
-	<h2 className={image_heading_clases}>
-	FIGHT THE SYSTEM
-	</h2>
-	<h3 className={image_subheading_clases}>
-	ITS SERVANTS: DARK GUARDS & EXTRACTORS
-	</h3>
-	
-	<img src={placeholder_image_two}
-	alt="A lovely background" 
-	className="border-8 border-in-grey shadow-lg">
-	</img>
-	<h2 className={image_heading_clases}>
-	SOLVE THE MYSTERY
-	</h2>
-	<h3 className={image_subheading_clases}>
-	WHO'S THE MOTHER OF THIS PLANET?
-	</h3>
-	<img src={placeholder_image_three}
-	alt="A lovely background" 
-	className="border-8 border-in-grey shadow-lg">
-	</img>
+const ShowImgs = (props) =>  (	 
+		<div>
+		<h2 className={image_heading_clases}>
+		EXPLORE THE LAND
+		</h2>
+		<h3 className={image_subheading_clases}>
+		CAN YOU SPEAK TO CRYSTALS?
+		</h3>
+		<img src={placeholder_image_one}
+		alt="A lovely background" 
+		className="border-8 border-in-grey shadow-lg">
+		</img>
+		<h2 className={image_heading_clases}>
+		FIGHT THE SYSTEM
+		</h2>
+		<h3 className={image_subheading_clases}>
+		ITS SERVANTS: DARK GUARDS & EXTRACTORS
+		</h3>
+		
+		<img src={placeholder_image_two}
+		alt="A lovely background" 
+		className="border-8 border-in-grey shadow-lg">
+		</img>
+		<h2 className={image_heading_clases}>
+		SOLVE THE MYSTERY
+		</h2>
+		<h3 className={image_subheading_clases}>
+		WHO'S THE MOTHER OF THIS PLANET?
+		</h3>
+		<img src={placeholder_image_three}
+		alt="A lovely background" 
+		className="border-8 border-in-grey shadow-lg">
+		</img>
+		</div>
+	</div>
 	)
-
+	
 export default Game 
 
--- src/components/Play.js
+++ src/components/Play.js
@@ -11,8 +11,6 @@ const all_ties_down_text = (`Equipped with a black tie and a white collar shirt,
   whispers to you from the depths of your soul, you agree, and your journey into the 
   unconscious begins...`)
 
-  
-
 const Play = (props) =>  (
   <section className="text-white my-32">
   <header>
@@ -20,16 +18,22 @@ const Play = (props) =>  (
   </header>
   <article className="xl:max-w-full xl:mx-64 xl:mb-48 lato fw-300 text-gray-400">
   <section>
-
+  <div>
   <Game title={'All\nTies\nDown'}
   year_of_release="Work in progress. We plan to release the early access version around X-mas 2020." 
   genres="Exploration turn-based mystery story-driven female-character counter-cultural" 
-  what_its_all_about={all_ties_down_text} />
+  what_its_all_about={all_ties_down_text} 
+  custom_class="all-ties-down"
+  showImgs="true"/>
 
   <Game title="I LOVE YOU, THEREFORE I NEED YOU TO DIE...AGAIN"
   year_of_release="2020, Christmas" 
   genres="Story-driven metaphysical shamanic" 
-  what_its_all_about="Description..." />
+  what_its_all_about="Description..." 
+  custom_class="i-love-you"
+  showImgs="false"/>
+
+  </div>
 
   </section>
   </article>
